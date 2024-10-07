770173150d08995eb78924680728b63597bdf5c8
pathumck <ckpathumck@gmail.com>
2024-10-07 22:58:20 +0530

    create module for spring web mvc

---
+++ src/main/java/lk/ijse/springmvc/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.springmvc;
+
+import lk.ijse.springmvc.config.WebAppConfig;
+import lk.ijse.springmvc.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
