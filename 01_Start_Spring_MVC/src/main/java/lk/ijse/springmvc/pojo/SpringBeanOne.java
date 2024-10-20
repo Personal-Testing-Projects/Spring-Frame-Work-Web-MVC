7af3e4ffdc98160be0abeef9bd6dff086c82da71
pathumck <ckpathumck@gmail.com>
2024-10-21 00:43:40 +0530

    add @Override annotation

--- 01_Start_Spring_MVC/src/main/java/lk/ijse/springmvc/pojo/SpringBeanOne.java
+++ 01_Start_Spring_MVC/src/main/java/lk/ijse/springmvc/pojo/SpringBeanOne.java
@@ -12,22 +12,27 @@ public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, Applicati
         System.out.println("SpringBeanOne : Instantiated");
     }
 
+    @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("SpringBeanOne : beanFactoryAware");
     }
 
+    @Override
     public void setBeanName(String s) {
         System.out.println("SpringBeanOne : BeanNameAware");
     }
 
+    @Override
     public void destroy() throws Exception {
         System.out.println("SpringBeanOne : DisposableBean");
     }
 
+    @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("SpringBeanOne : InitializingBean");
     }
 
+    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("SpringBeanOne : ApplicationContextAware");
     }
