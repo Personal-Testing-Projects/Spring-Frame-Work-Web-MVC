601d24093dc7e8d1127e9491fb845867ed410bb4
pathumck <ckpathumck@gmail.com>
2024-10-22 10:42:51 +0530

    check basic crud mappings

--- 04_Mapping_Spec/src/main/java/lk/ijse/mappingspec/controller/OneController.java
+++ 04_Mapping_Spec/src/main/java/lk/ijse/mappingspec/controller/OneController.java
@@ -1,7 +1,27 @@
 package main.java.lk.ijse.mappingspec.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/one")
 public class OneController {
+    @GetMapping
+    public String testOne() {
+        return "Get method invoked";
+    }
+
+    @PostMapping
+    public String testTwo() {
+        return "Post method invoked";
+    }
+
+    @PutMapping
+    public String testThree() {
+        return "Put method invoked";
+    }
+
+    @DeleteMapping
+    public String testFour() {
+        return "Delete method invoked";
+    }
 }
