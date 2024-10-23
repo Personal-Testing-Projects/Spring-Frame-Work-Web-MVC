f48da56133e1f620385f97214b7e36a19c5da6d8
pathumck <ckpathumck@gmail.com>
2024-10-23 17:06:46 +0530

    check recieving data to rest controller in x-www-form-url encoded data typing using dto

--- 04_Mapping_Spec/src/main/java/lk/ijse/mappingspec/controller/CustomerPOSController.java
+++ 04_Mapping_Spec/src/main/java/lk/ijse/mappingspec/controller/CustomerPOSController.java
@@ -1,13 +1,20 @@
 package main.java.lk.ijse.mappingspec.controller;
 
+import main.java.lk.ijse.mappingspec.dto.CustomerDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 @CrossOrigin
 public class CustomerPOSController {
-    @PostMapping
+    /*@PostMapping
     public void saveCustomer(@RequestParam("id") String id, @RequestParam("name") String name,@RequestParam("address") String address, @RequestParam("phone") String phone) {
         System.out.println("id: " + id + " name: " + name + " address: " + address + " phone: " + phone);
+    }*/
+
+    @PostMapping
+    public void saveCustomer(CustomerDTO customerDTO) {
+        System.out.println(customerDTO.getId() + " " + customerDTO.getName() + " " +
+                customerDTO.getAddress() + " " + customerDTO.getPhone());
     }
 }
