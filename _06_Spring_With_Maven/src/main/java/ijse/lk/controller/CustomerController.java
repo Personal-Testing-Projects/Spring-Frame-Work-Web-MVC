5197515563c343c4d4506d110852db258869fa93
pathumck <ckpathumck@gmail.com>
2024-10-25 20:41:23 +0530

    create crud methods and check responses

--- _06_Spring_With_Maven/src/main/java/ijse/lk/controller/CustomerController.java
+++ _06_Spring_With_Maven/src/main/java/ijse/lk/controller/CustomerController.java
@@ -1,14 +1,38 @@
 package ijse.lk.controller;
 
 import ijse.lk.dto.CustomerDTO;
+import ijse.lk.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/customer")
 @CrossOrigin
 public class CustomerController {
     @PostMapping
-    public void getCustomer(@RequestBody CustomerDTO customerDTO) {
+    public ResponseUtil saveCustomer(@RequestBody CustomerDTO customerDTO) {
         System.out.println(customerDTO.toString());
+        return new ResponseUtil("OK", "Successfully Added", null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteCustomer(@RequestParam("id") String id) {
+        return new ResponseUtil("OK", "Successfully Deleted : " + id, null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        System.out.println(customerDTO.toString());
+        return new ResponseUtil("OK", "Successfully Updated : " + customerDTO.getId(), null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllCustomers() {
+        ArrayList<CustomerDTO> customerDTOS = new ArrayList<>();
+        customerDTOS.add(new CustomerDTO("C001", "Sam", "Dhaka", "01711111111"));
+        customerDTOS.add(new CustomerDTO("C002", "Sam", "Dhaka", "01711111111"));
+        customerDTOS.add(new CustomerDTO("C003", "Sam", "Dhaka", "01711111111"));
+        return new ResponseUtil("OK", "All Customers", customerDTOS);
     }
 }
