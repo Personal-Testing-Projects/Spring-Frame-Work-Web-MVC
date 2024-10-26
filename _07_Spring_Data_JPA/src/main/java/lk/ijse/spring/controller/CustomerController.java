639c36d98f18720eea6072e81cda41bb4029dad1
pathumck <ckpathumck@gmail.com>
2024-10-26 20:43:51 +0530

    test save customer through customer repo

--- _07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ _07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ public class CustomerController {
         if (customerDTO.getId().equals("C001")) {
             throw new RuntimeException("Customer Already Exists");
         }
+        customerRepo.save(new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress(), customerDTO.getPhone()));
         return new ResponseUtil("OK", "Successfully Added", null);
     }
 
