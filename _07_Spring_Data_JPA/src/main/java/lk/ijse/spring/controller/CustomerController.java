dce86d1d89f9995b9a2801e4b4d816a278ca40e8
pathumck <ckpathumck@gmail.com>
2024-10-26 23:24:53 +0530

    update crud methods with spring jpa

--- _07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ _07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -4,11 +4,14 @@ import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.util.ResponseUtil;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -16,40 +19,50 @@ import java.util.ArrayList;
 public class CustomerController {
     @Autowired
     private CustomerRepo customerRepo;
+    @Autowired
+    private ModelMapper modelMapper;
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseUtil saveCustomer(@RequestBody CustomerDTO customerDTO) {
         //System.out.println(customerDTO.toString());
+/*
         if (customerDTO.getId().equals("C001")) {
             throw new RuntimeException("Customer Already Exists");
         }
-        customerRepo.save(new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress(), customerDTO.getPhone()));
+*/
+        if (customerRepo.existsById(customerDTO.getId())) {
+            throw new RuntimeException("Customer Already Exists");
+        }
+        customerRepo.save(modelMapper.map(customerDTO, Customer.class));
         return new ResponseUtil("OK", "Successfully Added", null);
     }
 
     @DeleteMapping(params = "id")
     public ResponseUtil deleteCustomer(@RequestParam("id") String id) {
-        if (id.equals("C001")) {
-            throw new RuntimeException("Customer Already Deleted");
+        if (!customerRepo.existsById(id)) {
+            throw new RuntimeException("Wrong ID");
         }
+        customerRepo.deleteById(id);
         return new ResponseUtil("OK", "Successfully Deleted : " + id, null);
     }
 
     @PutMapping
     public ResponseUtil updateCustomer(@RequestBody CustomerDTO customerDTO) {
         //System.out.println(customerDTO.toString());
-        if (customerDTO.getId().equals("C001")) {
+        /*if (customerDTO.getId().equals("C001")) {
             throw new RuntimeException("Wrong ID");
+        }*/
+        if (!customerRepo.existsById(customerDTO.getId())) {
+            throw new RuntimeException("Customer Already Exists");
         }
+        customerRepo.save(modelMapper.map(customerDTO, Customer.class));
         return new ResponseUtil("OK", "Successfully Updated : " + customerDTO.getId(), null);
     }
 
     @GetMapping
     public ResponseUtil getAllCustomers() {
-        ArrayList<CustomerDTO> customerDTOS = new ArrayList<>();
-        customerDTOS.add(new CustomerDTO("C001", "Sam", "Dhaka", "01711111111"));
-        customerDTOS.add(new CustomerDTO("C002", "Sam", "Dhaka", "01711111111"));
-        customerDTOS.add(new CustomerDTO("C003", "Sam", "Dhaka", "01711111111"));
+        List<Customer> customers = customerRepo.findAll();
+        ArrayList<CustomerDTO> customerDTOS = modelMapper.map(customers, new TypeToken<ArrayList<CustomerDTO>>(){}.getType());
         return new ResponseUtil("OK", "All Customers", customerDTOS);
     }
 }
